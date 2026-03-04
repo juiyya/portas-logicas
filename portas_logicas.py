9de4fce302f3d5bbf1924db089a6df6d0867d33d
juiyya <forgetjuya@outlook.com>
2026-03-03 22:05:05 -0300

    Estudo sobre portas_logicas.

---
+++ portas_logicas.py
@@ -0,0 +1,15 @@
+A = 0
+B = 0
+C = 0
+D = 0
+
+print("A B C D | X")
+for A in range(2):
+  for B in range(2):
+    for C in range(2):
+     for D in range(2):
+         X = 0
+         if A and B:
+          X = 1
+
+    print(A,B,C,D,'|', X)
